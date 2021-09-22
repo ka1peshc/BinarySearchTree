62c7fd3b81b0d70db41f047bc22640fb157471df
ka1peshc <kalpesh111997@gmail.com>
2021-09-23 01:01:51 +0530

    Added ten more node in binary tree

--- BinnarySearchTree/Program.cs
+++ BinnarySearchTree/Program.cs
@@ -11,7 +11,16 @@ namespace BinnarySearchTree
             BT.InsertNode(56);
             BT.InsertNode(30);
             BT.InsertNode(70);
-            
+            BT.InsertNode(22);
+            BT.InsertNode(40);
+            BT.InsertNode(11);
+            BT.InsertNode(3);
+            BT.InsertNode(16);
+            BT.InsertNode(60);
+            BT.InsertNode(95);
+            BT.InsertNode(65);
+            BT.InsertNode(63);
+            BT.InsertNode(67);
             Node<int> root = BT.GetRoot();
             BT.Inorder(root);
         }
